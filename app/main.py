b43ef97cae0416b3f132b43e3586691eae4c930c
keaunsol Na <knsol1992@naver.com>
2025-12-10 00:34:24 +0900

    feat: [나큰솔] 불특정 다수의 사용자는 카카오 로그인을 통해 로그인을 할 수 있다. [NB-54]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 from finance.adapter.input.web.finance_router import finance_router
+from kakao_authentication.adapter.input.web.kakao_authentication_router import kakao_authentication_router
 from market_data.adapter.input.web.market_data_router import market_data_router
 
 load_dotenv()
@@ -57,6 +58,8 @@ app.include_router(ie_info_router, prefix="/ie_info")
 app.include_router(product_data_router, prefix="/product")
 app.include_router(market_data_router, prefix="/market-data")
 app.include_router(finance_router, prefix="/finance")
+app.include_router(kakao_authentication_router, prefix="/kakao-authentication")
+
 # 앱 실행
 
 if __name__ == "__main__":
